85fef5112cf7dd93bfb7c4967d5884065d60dd76
wenF7752 <113209938+wenF7752@users.noreply.github.com>
2024-11-02 18:32:42 -0400

    refactore conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,10 @@
-import "dotenv/config"
-import { MongoClient } from "mongodb";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const connectionString = process.env.ATLAS_URI || "";
 
-const client = new MongoClient(connectionString);
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to Mongo"))
+  .catch((err) => console.log(err));
 
-let conn;
-try { 
-  conn = await client.connect()
-  console.log("Connected to Mongo")
-} catch (err) {
-  console.log(err)
-}
-
-const db = await conn.db("sample_training");
-
-export default db
+export default mongoose;
